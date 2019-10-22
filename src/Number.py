41c279f926e486a392b4b8d25688a44341087c24
KMDMNAK <kmdmuneaki@gmail.com>
2019-10-22 13:38:49 +0900

    update  complex

---
+++ src/Number.py
@@ -0,0 +1,22 @@
+"""
+this module contain complex number handler and complex fraction number
+"""
+class Number:
+    """
+        Fraction
+        double
+        int
+        Complex
+        Fraction
+    """
+    pass
+
+class Complex:
+    def __init__(self, real, imag):
+        """
+            real : Number
+            imag : Number
+        """
+        self.imag = imag
+        self.real = real
+        
---
+++ src/test.py
@@ -0,0 +1,23 @@
+import algorithm
+
+"""
+5x^4 + 3x^3 +1 x^2 +0 +5
+=>
+[5,3,1,0,5]
+
+coeffiecient=[5,3,1,0,5]
+Q_coeffiecient=[3,1,0,5]
+"""
+
+"""
+#H = algorithm.HurwitzStabililtyTestForRealPolymonials([5,3,1,0,5])
+H = algorithm.HurwitzStabililtyTestForRealPolymonials([-1, 0, 1])
+print(H.execute())
+print(H.P_array)
+"""
+
+H = algorithm.HurwitzStabililtyTestForComplexPolymonials([
+    1+1j, 2+3j, 4+6j,4+1j
+])
+print(H.execute())
+print(H.P_array)
